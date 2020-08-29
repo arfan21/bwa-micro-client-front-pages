aa228d01f69efa5433813c238c185d4f3164fb13
arfan21 <arfan.jadul10@gmail.com>
2020-08-29 21:21:32 +0700

    initial commit

---
+++ src/parts/ListCourses/RenderItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+import IconPlay from "public/images/icon-play.svg";
+import Link from "next/link";
+
+export default function RenderItem({ item }) {
+    console.log(item);
+    return (
+        <div className="w-1/4 px-4">
+            <div className="item relative">
+                <figure className="item-image">
+                    <IconPlay></IconPlay>
+                    <img
+                        src={item?.thumbnail ?? ""}
+                        alt={item?.name ?? "some information"}
+                    />
+                </figure>
+                <div className="item-meta mt-2">
+                    <h4 className="text-lg text-gray-900">
+                        {item?.name ?? "Course Name"}
+                    </h4>
+                    <h5 className="text-sm text-gray-600">
+                        {item?.level ?? "Course Level"}
+                    </h5>
+                </div>
+                <Link href="/course/[id]" as={`/course/${item.id}`}>
+                    <a className="link-wrapped"></a>
+                </Link>
+            </div>
+        </div>
+    );
+}
